5ca45a248c634d7eb24ab792ebf663e6966addd7
Thomas Bell <thomas@thomasbell.io>
2025-05-19 17:32:38 -0600

    added environment variable

--- enter.sh
+++ enter.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/zsh
-docker exec -ti  -w /home/$USER_NAME/code/$(basename $PWD) -u "$USER_NAME" $CONTAINER_NAME zsh -l
+docker exec -ti  -e SSH_AUTH_SOCK=${SSH_AUTH_SOCK}  -w /home/$USER_NAME/code/$(basename $PWD) -u "$USER_NAME" $CONTAINER_NAME zsh -l
